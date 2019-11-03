2ec7809c44dca42ff84b91c291d9364d272de310
Jimmy Morrens <jimmy@MacBook-Pro-de-Jimmy.local>
2019-11-03 15:42:53 +0100

    first build

---
+++ build/precache-manifest.946405419a23705aa027c894ed2557bc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170804cedfc755f70d2c1ad8e1ac7f9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "744d852644648010d4e5",
+    "url": "/static/css/main.db5883d7.chunk.css"
+  },
+  {
+    "revision": "b1122caffa79f4138270",
+    "url": "/static/js/2.e257533e.chunk.js"
+  },
+  {
+    "revision": "744d852644648010d4e5",
+    "url": "/static/js/main.2b7391fd.chunk.js"
+  },
+  {
+    "revision": "996602fed52a1be2715f",
+    "url": "/static/js/runtime-main.94606cdd.js"
+  },
+  {
+    "revision": "27add3dcb6d5485daf5e445e72f02cd4",
+    "url": "/static/media/bg.27add3dc.jpg"
+  },
+  {
+    "revision": "940323bcd2d98d5507777f76c01a51e6",
+    "url": "/static/media/drizzle.940323bc.svg"
+  },
+  {
+    "revision": "3c6ec9f36d2d82f19d3e1c16ffced24b",
+    "url": "/static/media/humidity.3c6ec9f3.svg"
+  },
+  {
+    "revision": "3936f3a45f22fc03939ab62559a12430",
+    "url": "/static/media/search.3936f3a4.svg"
+  },
+  {
+    "revision": "628eeebd0b79a1e7d7221e01d8b601f9",
+    "url": "/static/media/sun.628eeebd.svg"
+  },
+  {
+    "revision": "4c64ae385c166764bcadbe4167c6ddaa",
+    "url": "/static/media/sunset.4c64ae38.svg"
+  },
+  {
+    "revision": "d5793c9e91a15d33465d79462a441be7",
+    "url": "/static/media/wind.d5793c9e.svg"
+  }
+]);
